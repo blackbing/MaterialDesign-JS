908084f034752362d1aa3ae7f14aed675ee7b2c6
Bingo Yang <yangbi@Bingo-Yang.local>
2022-10-25 13:16:01 +0800

    support es module

--- build.js
+++ build.js
@@ -9,10 +9,13 @@ const convert =  function(matches){
 
 const lines = meta.map(icon => {
   let name = icon.name.replace(find, convert);
-  name = `${name[0].toUpperCase()}${name.slice(1)}`;
-  return `export const mdi${name}: string = "${icon.path}";`;
+  name = `mdi${name[0].toUpperCase()}${name.slice(1)}`;
+  util.write(`./es/${name}.js`, `export default "${icon.path}"`);
+
+  // return `export const mdi${name}: string = "${icon.path}";`;
+  return `export { default as ${name} } from "./es/${name}.js";`;
 });
 
 const output = `// Material Design Icons v${util.getVersion()}\n${lines.join('\n')}`;
 
-util.write("mdi.ts", output);
+util.write("mdi.js", output)
